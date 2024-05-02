f55723b53795422c5b28e85b607417c3193c54c3
faom002 <faisal_jobb@outlook.com>
2024-05-02 01:51:02 -0700

    added some lines of code

--- obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ead3fac5e611c3aea3700d2181c1d01616116bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3286f54f8fca580c56079ace3f6b1ace689fd69a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
